4c727a2d5e4859305e29dfeb7f63e4b92560b404
Ryan Moore <moorer@udel.edu>
2018-05-29 17:35:38 -0400

    Update api docs

---
+++ docs/search/defines_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['rstring_5farray_5fbad',['rstring_array_bad',['../rstring_8h.html#ac4678df0af869b567c70cb475596b34f',1,'rstring.h']]],
+  ['rstring_5fbad',['rstring_bad',['../rstring_8h.html#ac4cb3fc44c65dc2502a7e6e264a0dcd3',1,'rstring.h']]],
+  ['rstring_5fdata',['rstring_data',['../rstring_8h.html#a1d3e209a626e8fb9d40108626d916ba3',1,'rstring.h']]],
+  ['rstring_5fformat',['rstring_format',['../rstring_8h.html#ab34abe7edbbc230edf9f2078fa19182c',1,'rstring.h']]]
+];
--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['rfile_2ec',['rfile.c',['../rfile_8c.html',1,'']]],
-  ['rstring_2ec',['rstring.c',['../rstring_8c.html',1,'']]]
+  ['rstring_2ec',['rstring.c',['../rstring_8c.html',1,'']]],
+  ['rstring_2eh',['rstring.h',['../rstring_8h.html',1,'']]]
 ];
